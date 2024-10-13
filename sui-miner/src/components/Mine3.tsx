d0e5118f1ba76888a8a1f85d7c7ea28c69027a2a
Maxime Delhaye <maxime.delhaye28@gmail.com>
2024-10-13 08:48:38 +0200

    pull please

--- sui-miner/src/components/Mine3.tsx
+++ sui-miner/src/components/Mine3.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import '../styles/Mine3.css'; // Importer le fichier CSS
+import MinerAnimation from '../animation/AnimationMiner';
 import ManagerAnimation from '../animation/ManagerAnimation';
 const Mine3: React.FC = () => {
+    function toggleLock() {
+        const lock = document.querySelector('.Locked');
+        if (lock) {
+            lock.classList.remove('Locked');
+        }
+    }
   return (
     <div className="mine2-background Locked">
-        <ManagerAnimation />
-        <img className='imgLock' src="../../public/password.png" alt="Locked" />
+        <div className='manager3' >
+            <ManagerAnimation />
+            <MinerAnimation/>
+        </div>
+       
+        <button className='imgLock' onClick={toggleLock} />
       {/* Contenu du composant */}
     </div>
   );
---
+++ sui-miner/src/components/Miner.tsx
@@ -0,0 +1,10 @@
+import MinerAnimation from "../animation/AnimationMiner";
+
+
+const Miner: React.FC = () => {
+    return (
+      
+    );
+};
+  
+  export default Miner;
