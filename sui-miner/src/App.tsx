255a08374c40e69dc0f184e6e37ff4a2025eba5f
antoinayaya <antoine.mauroit@gmail.com>
2024-10-13 08:07:36 +0200

    Jbihb

--- sui-miner/src/App.tsx
+++ sui-miner/src/App.tsx
@@ -4,14 +4,52 @@ import Mine1 from './components/Mine1'
 import Mine2 from './components/Mine2'
 import Mines from './components/Mines'
 import Menu from './components/Menu'
-import React from 'react';
-import DrillAnimation from './DrillAnimation';
-import MinerAnimation from './MinerAnimation';
-import CartAnimation from './CartAnimation';
-import ManagerAnimation from './ManagerAnimation';
+import React, { useState, useEffect } from 'react';
+
 
 
 function App() {
+  const [argent, setArgent] = useState(0);
+  const [argent_payant, setArgent_Payant] = useState(10000);
+  const [niveauAmelioration, setNiveauAmelioration] = useState(10);
+  const [coutAmelioration1, setCoutAmelioration1] = useState(10);
+  const [levelMine1, setLevelMine1] = useState(1);
+  const [levelManager1, setLevelManager1] = useState(0);
+  const [levelMine2, setLevelMine2] = useState(0);
+  const [coutAmelioration2, setCoutAmelioration2] = useState(100);
+  const [coutManager1, setCoutManager1] = useState(300);
+  const [niveauBoost, setNiveauBoost] = useState(1);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setArgent(argent => argent + niveauAmelioration*niveauBoost);
+    }, 1000);
+    return () => clearInterval(intervalId);
+  }, [niveauAmelioration]);
+  const handleAcheterAmelioration1 = () => {
+    if (argent >= coutAmelioration1) {
+      setArgent(argent - coutAmelioration1);
+      setNiveauAmelioration(niveauAmelioration + 5);
+      setCoutAmelioration1(coutAmelioration1 * 2); // Double le coût de l'amélioration à chaque niveau
+      setLevelMine1(levelMine1 + 1);
+    }
+  };
+    const handleAcheterAmelioration2 = () => {
+      if (argent >= coutAmelioration2) {
+        setArgent(argent - coutAmelioration2);
+        setNiveauAmelioration(niveauAmelioration + 50);
+        setCoutAmelioration2(coutAmelioration2 * 2);
+        setLevelMine2(levelMine2 + 1); // Double le coût de l'amélioration à chaque niveau
+      }
+  };
+  const handleManager1 = () => {
+    if (argent_payant >= coutManager1) {
+      setArgent_Payant(argent_payant - coutManager1);
+      setNiveauBoost(niveauBoost +1);
+      setCoutManager1(coutManager1 * 10); // Double le coût de l'amélioration à chaque niveau
+      setLevelManager1(levelManager1 + 1);
+    }
+};
   return (
     
     <>
@@ -29,10 +67,19 @@ function App() {
       </Routes>
     </Router>
       <div id="gameContainer">
-      <DrillAnimation />
-      <MinerAnimation />
-      <CartAnimation />
-      <ManagerAnimation />
+      <div>
+      <p>Vous avez {argent} pièces.</p>
+      <p>Vous avez {argent_payant} SuiCoins.</p>
+      <button onClick={handleAcheterAmelioration1}>
+        Acheter une amélioration Mine 1({coutAmelioration1} pièces) Level : ({levelMine1})
+      </button>
+      <button onClick={handleAcheterAmelioration2}>
+        Acheter une amélioration Mine 2({coutAmelioration2} pièces) Level : ({levelMine2})
+      </button>
+      <button onClick={handleManager1}>
+        Acheter un manager pour la mine 1({coutManager1} pièces) Level : ({levelManager1})
+      </button>
+      </div>
     
         <Mines/>
         <Mine1/>
--- sui-miner/src/index.ts
+++ sui-miner/src/index.ts
@@ -21,6 +21,30 @@ class Manager {
 
 }
 
+class Pusher {
+    private level: number;
+    private cost: number;
+    private initialCost: number;
+    private boost:number;
+
+    constructor(level = 1, cost = 100, initialCost = 100, boost = 1){
+        this.level = level;
+        this.cost = cost;
+        this.initialCost = initialCost;
+        this.boost = boost;
+    }
+
+    upgrade(){
+        this.level += 1;
+    }
+    newCost(){
+        this.cost = this.initialCost * (this.level**1.20);
+    }
+    newBoost(){
+        this.boost = 1*this.level  ;
+    }
+}
+
 class Mine {
     private level: number;
     private mineCost: number;
@@ -49,25 +73,33 @@ class Mine {
 
 }
 
-const Mine1 = new Mine();
-const Mine2 = new Mine();
-const Mine3 = new Mine();
-const Mine4 = new Mine();
-const Mine5 = new Mine();
-Mine1.constructor(1,10,10,10,10,10);
-Mine2.constructor(100,1000,1000,1000,1000,1000);
-Mine3.constructor(10000,100000,100000,100000,100000,100000);
-Mine4.constructor(1000000,10000000,10000000,10000000,10000000,10000000);
-Mine5.constructor(100000000,1000000000,1000000000,1000000000,1000000000);
-const Manager1 = new Manager();
-const Manager2 = new Manager();
-const Manager3 = new Manager();
-const Manager4 = new Manager();
-const Manager5 = new Manager();
-Manager1.constructor(100,2,false);
-Manager2.constructor(10000,2,false);
-Manager3.constructor(1000000,2,false);
-Manager4.constructor(100000000,2,false);
-Manager5.constructor(10000000000,2,false);
-  
-  
+
+    const Mine1 = new Mine();
+    const Mine2 = new Mine();
+    const Mine3 = new Mine();
+    const Mine4 = new Mine();
+    const Mine5 = new Mine();
+
+
+        Mine1.constructor(1,10,10,10,10,10);
+        Mine2.constructor(100,1000,1000,1000,1000,1000);
+        Mine3.constructor(10000,100000,100000,100000,100000,100000);
+        Mine4.constructor(1000000,10000000,10000000,10000000,10000000,10000000);
+        Mine5.constructor(100000000,1000000000,1000000000,1000000000,1000000000);
+    Mine1.constructor(1,10,10,10,10,10);
+    Mine2.constructor(100,1000,1000,1000,1000,1000);
+    Mine3.constructor(10000,100000,100000,100000,100000,100000);
+    Mine4.constructor(1000000,10000000,10000000,10000000,10000000,10000000);
+    Mine5.constructor(100000000,1000000000,1000000000,1000000000,1000000000);
+    const Manager1 = new Manager();
+    const Manager2 = new Manager();
+    const Manager3 = new Manager();
+    const Manager4 = new Manager();
+    const Manager5 = new Manager();
+    Manager1.constructor(100,2,false);
+    Manager2.constructor(10000,2,false);
+    Manager3.constructor(1000000,2,false);
+    Manager4.constructor(100000000,2,false);
+    Manager5.constructor(10000000000,2,false);
+    const pusher = new Pusher ();
+    pusher.constructor(1,100,100,1);
